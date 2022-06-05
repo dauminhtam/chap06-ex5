ea08d95bf77df821b65bd38f445649ae68e6677b
dauminhtam <minhtam745@gmail.com>
2022-06-05 14:15:41 +0700

    Create chap06-ex5.c

---
+++ chap06-ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 80;
+	char b = 'C';
+	printf ("\nThis is number stored in 'a' %d", a);
+	printf ("\nThis is charater interpreted from 'a' %c", a);
+	printf ("\nThis is charater stored in 'C' %c", b);
+	printf ("\nThis is number stored in 'C' %d", b);
+	return 0;
+}
